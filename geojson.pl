b2265c3cbb60fb3697eb04f28eae22a077fd0c10
Stuart Lowe <gravatar@strudel.org.uk>
2019-06-20 15:52:03 +0100

    Update GeoJSON

--- geojson.pl
+++ geojson.pl
@@ -9,17 +9,14 @@ if(!$place){
 }
 
 $lafile = "LA.geojson";
-$status = "status.csv";
+$status = "index.csv";
 
-if((time - (stat $status)[9]) > 86400 || -s $status == 0){
-	`wget -q -O $status "https://docs.google.com/spreadsheets/u/0/d/1Tsmv6m6oA6R6z5y_cvts6Tr_Vb_8zBJri4IXWBnD9CA/export?format=csv&id=1Tsmv6m6oA6R6z5y_cvts6Tr_Vb_8zBJri4IXWBnD9CA&gid=0"`;
-}
 open(FILE,$status);
 @lines = <FILE>;
 close(FILE);
 $code = "";
 foreach $line (@lines){
-	if($line =~ /$place\,([0-9A-Z]+)\,/i){
+	if($line =~ /^([0-9A-Z]+)\,$place/i){
 		$code = $1;
 	}
 }
@@ -39,6 +36,7 @@ if($lines[0] =~ /(\{"type":"Feature","properties":\{"objectid":[0-9]+,"lad19cd":
 	$json = "{\"type\":\"FeatureCollection\",\"features\":[$1]}\n";
 	$json =~ s/([0-9]\.[0-9]{5})[0-9]+/$1/g;
 }
+$place =~ s/\s/-/g;
 open(FILE,">","data/$place.geojson");
 print FILE $json;
 close(FILE);
